f3f585e6131348708117947755590059ee0732cc
Thomas Claireau <thomas.claireau@gmail.com>
2020-06-08 19:06:00 +0200

    clean app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'App',
+};
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Lilita+One&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Lilita One', Helvetica, Arial, sans-serif;
+}
+
+*,
+*:before,
+*:after {
+	box-sizing: border-box;
 }
 </style>
